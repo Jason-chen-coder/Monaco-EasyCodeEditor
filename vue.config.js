4578c37d10857d626062b7aeea588a266c03965b
节省钱 <53551357+Jason-chen-coder@users.noreply.github.com>
2021-09-24 15:17:25 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/Monaco-EasyCodeEditor/'
-    : '/',
+  publicPath: process.env.NODE_ENV === 'production'? '/Monaco-EasyCodeEditor/': '/',
   outputDir: 'dist',
   lintOnSave: true,
   configureWebpack: {
